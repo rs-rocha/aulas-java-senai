a5e4421b4653f6cae8d520b331fabdb0d4960353
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-09 20:57:51 -0300

    Atualiza Segunda Aula

--- src/Formulario.java
+++ src/Formulario.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Formulario {
-    
+
     public static void main(String[] args) {
 
         String nomeCompleto;
--- src/SegundaAula.java
+++ src/SegundaAula.java
@@ -1,2 +1,5 @@
 public class SegundaAula {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!");
+    }
 }
