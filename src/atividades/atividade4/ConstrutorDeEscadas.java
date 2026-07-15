51a9668c89fe0284aae0e7b50fc0005ef6122d65
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-15 19:30:24 -0300

    Implementa a resolução do ConstrutorDeEscadas

--- src/atividades/atividade4/ConstrutorDeEscadas.java
+++ src/atividades/atividade4/ConstrutorDeEscadas.java
@@ -1,12 +1,21 @@
 package atividades.atividade4;
 
+import java.util.Scanner;
+
 public class ConstrutorDeEscadas {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int qtdEscadas;
+        String simbolo;
+        System.out.println("Digite o simbolo para construir a escada: ");
+        simbolo = sc.nextLine();
+        System.out.println("Digite a quantidade de escadas: ");
+        qtdEscadas = sc.nextInt();
 
-        char simbolo = '*';
-        for(int i = 1; i <= 5; i++){
-            for(int j = 1; j <= 5; j++) {
-                System.out.printf("%s", simbolo);
+        for(int i = 1; i <= qtdEscadas; i++){
+            for(int j = 1; j <= qtdEscadas; j++) {
+                if (j < i)
+                    System.out.printf("%s", simbolo);
             }
             System.out.printf("%s\n", simbolo);
         }
