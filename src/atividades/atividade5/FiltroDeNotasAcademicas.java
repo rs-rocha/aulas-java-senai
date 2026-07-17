2d464267db41e636ba062e04fd44b54dddd0ee14
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-16 21:45:21 -0300

    Inicio da implementacao

--- src/atividades/atividade5/FiltroDeNotasAcademicas.java
+++ src/atividades/atividade5/FiltroDeNotasAcademicas.java
@@ -1,4 +1,11 @@
 package atividades.atividade5;
 
+import java.util.ArrayList;
+
 public class FiltroDeNotasAcademicas {
+
+    public static void main(String[] args) {
+
+        ArrayList<Double> notas = new ArrayList<>();
+    }
 }
