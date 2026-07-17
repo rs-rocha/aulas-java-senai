2a26b9e4e4ab8de013b6b618524c0a16404de773
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-16 21:29:29 -0300

    Adiciona a classe MelhoriaDeTarefas da atividade 6 exercicio 1

---
+++ src/atividades/atividade6/MelhoriaDeTarefas.java
@@ -0,0 +1,16 @@
+package atividades.atividade6;
+
+public class MelhoriaDeTarefas {
+    public static void main(String[] args) {
+        /*
+         * Ordenação de tarefas em ordem alfabetica/tadas
+         * Metodo para remover tarefas espefícicas
+         *  > Tarefas pelo metodo FIFO
+         *  > Tarefas pelo metodo FEFO
+         *  > Tarefas pelo metodo LIFO
+         *
+         * (Opicional) Adicionar um metodo que registre
+         * as tarefas que foram concluídas
+         */
+    }
+}
