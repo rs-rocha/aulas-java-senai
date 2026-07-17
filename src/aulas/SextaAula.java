9d2a042c194a95157dd5aa5ef87fa9b9752aca46
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-17 20:44:20 -0300

    Cria e implementa a sexta aula

---
+++ src/aulas/SextaAula.java
@@ -0,0 +1,13 @@
+package aulas;
+
+
+import aulas.objeto.Pessoa;
+
+public class SextaAula {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Joao";
+
+        System.out.println(pessoa1.nome);
+    }
+}
---
+++ src/aulas/objeto/Pessoa.java
@@ -0,0 +1,8 @@
+package aulas.objeto;
+
+public class Pessoa {
+    public String nome;
+    public int anoNascimento;
+    public char sexualidade;
+    public String cor;
+}
