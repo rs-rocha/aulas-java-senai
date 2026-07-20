4c578f38a1598b89d08a05afccf8085d1e82c2c6
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-20 12:10:46 -0300

    Implementa a primeira parte da Atividade 6

--- src/atividades/atividade6/MelhoriaDeTarefas.java
+++ src/atividades/atividade6/MelhoriaDeTarefas.java
@@ -1,16 +1,38 @@
 package atividades.atividade6;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Ordenação de tarefas em ordem alfabetica/tadas
+ * Metodo para remover tarefas espefícicas
+ *  > Tarefas pelo metodo FIFO
+ *  > Tarefas pelo metodo FEFO
+ *  > Tarefas pelo metodo LIFO
+ * (Opicional) Adicionar um metodo que registre
+ * as tarefas que foram concluídas
+ */
+
 public class MelhoriaDeTarefas {
     public static void main(String[] args) {
-        /*
-         * Ordenação de tarefas em ordem alfabetica/tadas
-         * Metodo para remover tarefas espefícicas
-         *  > Tarefas pelo metodo FIFO
-         *  > Tarefas pelo metodo FEFO
-         *  > Tarefas pelo metodo LIFO
-         *
-         * (Opicional) Adicionar um metodo que registre
-         * as tarefas que foram concluídas
-         */
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tarefas = new ArrayList<>();
+
+        System.out.println("Digite suas tarefas:");
+        while (true){
+            System.out.print("Nova tarefa: ");
+            tarefas.add(scanner.nextLine());
+
+            System.out.println("Quer adicionar mais alguma tarefa? (digite 'fim' para finalizar o programa)");
+            String opcao = scanner.nextLine();
+
+            if (opcao.equals("fim")){
+                break;
+            }
+        }
+        System.out.println("Tarefas salvas:");
+        for (int i = 0; i < tarefas.size(); i++) {
+            System.out.println("Tarefa nº"+(i+1)+" "+tarefas.get(i));
+        }
     }
 }
