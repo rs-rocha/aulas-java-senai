05eab1ff908de3e41b2861687ddac51b1f4ff7f1
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-20 20:02:26 -0300

    Implementa ContaBancaria.java e instancia o objeto em Main.java

--- src/aulas/encapsulamento/banco/modelo/ContaBancaria.java
+++ src/aulas/encapsulamento/banco/modelo/ContaBancaria.java
@@ -5,4 +5,38 @@ public class ContaBancaria {
     private String titular;
     private int numeroDaConta;
     private double saldo;
+
+    //Construtor dessa classe
+    public ContaBancaria(String titular, int numeroDaConta){
+        this.titular = titular;
+        this.numeroDaConta = numeroDaConta;
+        this.saldo = 0.00;
+    }
+
+    //metodos getters e setters
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
 }
--- src/aulas/encapsulamento/banco/principal/Main.java
+++ src/aulas/encapsulamento/banco/principal/Main.java
@@ -1,4 +1,15 @@
 package aulas.encapsulamento.banco.principal;
 
+import aulas.encapsulamento.banco.modelo.ContaBancaria;
+
 public class Main {
+    public static void main(String[] args) {
+        ContaBancaria minhaConta = new ContaBancaria(
+                "Joao",
+                123456
+        );
+
+
+
+    }
 }
